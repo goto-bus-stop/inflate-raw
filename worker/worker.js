d7f0178a05530254e32ed1f570f1f3d62b344284
Renée Kooi <renee@kooi.me>
2018-08-10 23:40:23 +0200

    transfer the ArrayBuffer, not the Uint8Array

--- worker/worker.js
+++ worker/worker.js
@@ -3,7 +3,7 @@ var inflate = require('pako/lib/inflate').inflateRaw
 
 self.addEventListener('message', function (ev) {
   try {
-    var uncompressed = inflate(ev.data)
+    var uncompressed = inflate(ev.data).buffer
     self.postMessage({ uncompressed: uncompressed }, [ev.data, uncompressed])
   } catch (err) {
     self.postMessage({ error: err })
